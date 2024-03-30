d6da0fd491b6cb5b6870803b41af0c92d14aac01
kiwi-jang <kiwi.jang@toss.im>
2024-03-31 02:49:35 +0900

    law-maker sample

---
+++ law-maker.ts
@@ -0,0 +1,24 @@
+export type LawMaker = {
+  id: string;
+  이름: string;
+  정당: string;
+  당선횟수: number;
+  소속위원회: string;
+  주요경력: string;
+  연락처: string;
+
+  본회의_출석률: number; // 백분율
+  본회의_출석률_등수: number;
+  상임위원회_출석률: {
+    위원회: string;
+    출석률: number; // 백분율
+    등수: number;
+  }[];
+
+  대표발의법안: number;
+  대표발의법안_등수: number;
+  위원회별_발의법안: {
+    위원회: string;
+    발의수: number;
+  }[];
+};
