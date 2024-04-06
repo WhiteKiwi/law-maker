eb6537d09459aa91419f17f11556d6b45d3e9466
kiwi-jang <jh145478@gmail.com>
2024-04-07 01:40:33 +0900

    feat: 주요법안표결 정렬

--- src/sync-style.ts
+++ src/sync-style.ts
@@ -1,5 +1,6 @@
 import { readJSON, readdir, writeJsonSync } from "fs-extra";
 import path from "path";
+import { LawMaker } from "./law-maker";
 
 async function main() {
   {
@@ -28,7 +29,15 @@ async function main() {
     const files = await readdir(lawMakerDir);
     for (const file of files) {
       const filePath = path.join(lawMakerDir, file);
-      const lawMaker = await readJSON(filePath);
+      const lawMaker: LawMaker = await readJSON(filePath);
+      // const 의안번호정렬 = [
+      //   2122268, 2102500, 2120877, 2107249, 2121515, 2123038, 2125809, 2119727,
+      //   2119142, 2126369, 2125837, 2120933,
+      // ];
+      // lawMaker.주요법안표결.sort(
+      //   (a, b) =>
+      //     의안번호정렬.indexOf(a.의안번호) - 의안번호정렬.indexOf(b.의안번호)
+      // );
       writeJsonSync(filePath, lawMaker, { spaces: 2 });
     }
   }
